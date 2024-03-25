d3fd92abb0ed5787311c5573a62bd1eae4ad5316
harunbd80 <python708280.gmail.com>
2024-03-26 00:48:19 +0600

    bangala_bazar

--- Main/urls.py
+++ Main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.home, name = 'home'),
     path('shop/',views.shop,name='shop'),
     path('shop-detail/',views.shop_detail,name='shop-detail'),
     path('cart/',views.cart,name='cart'),
